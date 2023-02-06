03d3879787f055a630e4ad78bceaff17620f39d9
BastienTLC <bastientalec@orange.fr>
2023-02-06 19:35:18 +0100

    three init projet ts

---
+++ src/three-scene/World/World.ts
@@ -0,0 +1,46 @@
+import * as THREE from 'three';
+import {Experience} from "../Experience";
+import {Camera} from "../Camera";
+import {Sizes} from "../Utils/Sizes";
+import {Scene} from "three";
+import {Floor} from "./Floor";
+import assets from "../Utils/assets";
+import {Resources} from "../Utils/Resources";
+import {Character} from "./character";
+import {Environment} from "./Environment";
+
+export class World {
+    private experience: Experience;
+    private sizes: Sizes;
+    private scene: Scene;
+    private canvas: HTMLCanvasElement;
+
+    private camera: Camera;
+    private renderer!: THREE.WebGLRenderer;
+    private floor!: Floor;
+    private resources: Resources;
+    private character!: Character;
+    private environment!: Environment;
+
+    constructor() {
+        this.experience = Experience.instance;
+        this.sizes = this.experience.sizes;
+        this.scene = this.experience.scene;
+        this.canvas = this.experience.canvas;
+        this.camera = this.experience.camera;
+        this.resources = new Resources(assets);
+
+        this.resources.on("ready", () =>{
+            this.environment = new Environment();
+            //this.floor = new Floor();
+            this.character = new Character();
+        })
+    }
+
+
+    resize(){
+    }
+
+    update(){
+    }
+}
---
+++ src/three-scene/World/character.ts
@@ -0,0 +1,33 @@
+import {Experience} from "../Experience";
+import {Scene} from "three";
+import * as THREE from 'three';
+import {Resources} from "../Utils/Resources";
+
+export class Character {
+    private experience: Experience;
+    private scene: Scene;
+    private resources: Resources;
+    private character: any;
+    private focusCharacter: any;
+
+    constructor() {
+        this.experience = Experience.instance;
+        this.scene = this.experience.scene;
+        this.resources = this.experience.resources;
+        this.character = this.resources.items["character"];
+        this.focusCharacter = this.character.scene;
+        this.setModel();
+    }
+
+    setModel(){
+        this.scene.add(this.focusCharacter);
+    }
+
+
+
+    resize(){
+    }
+
+    update(){
+    }
+}
